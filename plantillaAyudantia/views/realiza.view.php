4e9e218d2d0a8a62bab963b5f0c852dfe4f3530f
Diegojgl <67162892+Diegojgl@users.noreply.github.com>
2021-04-25 22:01:14 -0400

    proyectoiso

---
+++ plantillaAyudantia/views/realiza.view.php
@@ -0,0 +1,87 @@
+<!-- head -->
+<?php include('../partes/head.php') ?>
+    <!-- fin head -->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <!-- Styles -->
+     <link rel="stylesheet" href="../assets/css/otros.css">
+</head>
+
+<body>
+    <div class="d-flex" id="content-wrapper">
+      <!-- sideBar -->
+    <?php include('../partes/sidebar.php') ?>
+      <!-- fin sideBar -->
+
+        <div class="w-100">
+
+        <!-- Navbar -->
+        <?php include('../partes/nav.php') ?>
+          <!-- Fin Navbar -->
+
+        <!-- Page Content -->
+        <div id="content" class="bg-grey w-100">
+
+              <section class="bg-light py-3">
+                  <div class="container">
+                      <div class="row">
+                          <div class="col-lg-9 col-md-8">
+                            <h1 class="font-weight-bold mb-0">Formulario pedidos</h1>
+                          </div>
+                         
+                      </div>
+                  </div>
+              </section>
+
+
+              <section class="my-2 py-2">
+                  <div class="container">
+                    <div class="wrap">
+		                <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+			                <input type="text" class="form-control" name="nombre" id="nombre" placeholder="Nombre completo:" value="<?php if(!$enviado && isset($nombre)) echo $nombre?>">
+
+                            <input type="text" class="form-control" name="cargo" id="cargo" placeholder="cargo:" value="<?php if(!$enviado && isset($cargo)) echo $cargo?>">
+		
+			                <input type="text" class="form-control" name="correo" id="correo" placeholder="Correo:" value="<?php if(!$enviado && isset($correo)) echo $correo?>">
+		
+			                <textarea name="mensaje" class="form-control" id="mensaje" placeholder="Mensaje:"><?php if(!$enviado && isset($mensaje)) echo $mensaje?></textarea>
+		
+			                <?php if (!empty($errores)): ?>
+				            <div class="alert error" role="alert">
+					            <?php echo $errores; ?>
+				            </div>
+			                <?php elseif($enviado) : ?>
+				            <div class="alert success" role="alert">
+					            <?php echo 'Enviado Correctamente'; ?>
+				            </div>
+			                <?php endif; ?>
+		
+			                <input type="submit" name="submit" class="btn btn-primary" value="Enviar Solicitud">
+		                </form>
+	                 </div>
+                  </div>
+              </section>
+
+        </div>
+
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.3/dist/Chart.min.js" integrity="sha256-R4pqcOYV8lt7snxMQO/HSbVCFRPMdrhAFMH+vr9giYI=" crossorigin="anonymous"></script>
+</body>
+
+</html>
